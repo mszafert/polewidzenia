8b2b49cc429cc31b88f4d3f030006f2616a44ece
Marcin Szafert <mszafert@gmail.com>
2023-10-30 21:51:39 +0100

    ADD linting tools

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import eslint from '@rollup/plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [{ ...eslint({ include: 'src/**/*.+(ts|vue)' }), enforce: 'pre' }, vue()],
+  server: {
+    port: 3000,
+  },
+});
